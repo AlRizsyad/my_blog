b7d8bca77b947d8f848267efa2269f5c41856b71
AlRizsyad <malrizsyad@gmail.com>
2025-01-21 11:43:04 +0700

    Tutorial Laravel 9

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -4,12 +4,136 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class postController extends Controller
-{
+class PostController extends Controller
+{    
+    /**
+     * index
+     *
+     * @return void
+     */
     public function index()
     {
+        //get posts
         $posts = Post::latest()->paginate(5);
+
+        //render view with posts
         return view('posts.index', compact('posts'));
+    }
+    
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * store
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+    
+    /**
+     * edit
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+    
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, Post $post)
+    {
+        
+        $this->validate($request, [
+            'image'     => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        
+        if ($request->hasFile('image')) {
+
+            
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            
+            Storage::delete('public/posts/'.$post->image);
+
+            
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+
+        } else {
+
+            
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
+    }
+    
+    /**
+     * destroy
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function destroy(Post $post)
+    {
+        
+        Storage::delete('public/posts/'. $post->image);
+
+        
+        $post->delete();
+
+
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Dihapus!']);
     }
 }
